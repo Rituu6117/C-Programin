a84644dff1b019cb662d42226af53f7be92eff4f
Rituu6117 <umbrajeritesh@gmail.com>
2024-10-15 14:54:07 +0530

    Add files via upload

---
+++ ritesh2.c
@@ -0,0 +1,13 @@
+//loop.c
+//to print hello world  10 times 
+#include<stdio.h>
+int main ()
+{
+    int i =+1;
+    while (i<=10)//loop condition 
+    {
+        printf("%d\n",i);
+        i+=1;//update iterator  
+    }
+    return 0;
+}
